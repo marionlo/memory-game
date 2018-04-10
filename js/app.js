047e583bc9781f3d5e60cd7c3a7e3388eaa17ece
Marion-Lotito <marion.lotito@gmail.com>
2018-04-10 19:39:49 +0200

    feat: add card event listener

--- js/app.js
+++ js/app.js
@@ -1,32 +1,42 @@
- let icons = ["fa-anchor", "fa-anchor", "fa-bicycle", "fa-bicycle", "fa-bolt", "fa-bolt", "fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bomb", "fa-bomb"];
+let icons = ["fa-anchor", "fa-anchor", "fa-bicycle", "fa-bicycle", "fa-bolt", "fa-bolt", "fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bomb", "fa-bomb"];
+ let openCards = [];
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+   var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex != 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+   while (currentIndex != 0) {
+       randomIndex = Math.floor(Math.random() * currentIndex);
+       currentIndex -= 1;
+       temporaryValue = array[currentIndex];
+       array[currentIndex] = array[randomIndex];
+       array[randomIndex] = temporaryValue;
+   }
 
-    return array;
+   return array;
 }
 
 // Create the deck and shuffle the cards
 function newGame() {
 let cards = shuffle(icons);
-console.log(cards);
 for (let i = 0; i < cards.length; i++) {
 $( '.deck' ).append($('<li class="card"><i class="fa ' + cards[i] + '"></i></li>'))
 }
+   cardsListener();
 };
 
-newGame();
+// Listen to cards clicks
+function cardsListener() {
+console.log(openCards.length)
+ $(".card").click(function() {
+   if (openCards.length < 2) {
+       $(this).addClass('open show');
+        openCards.push(this);
+         console.log(openCards)
+   }
 
-// Listen to clicks on class card and show icons
-$( ".card" ).click(function(flip) {
-  $(this).addClass("show");
-});
+ }
+
+    )};
+newGame();
